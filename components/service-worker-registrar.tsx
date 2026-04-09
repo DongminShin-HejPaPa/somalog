d2ecea88ddc6355c31568d831fc94a36224657db
Dongmin Shin <beatlifedm@gmail.com>
2026-04-09 09:22:35 +0900

    feat: add PWA support - manifest, service worker, icons, apple-web-app meta

---
+++ components/service-worker-registrar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export function ServiceWorkerRegistrar() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => console.log("[SW] 등록 성공:", reg.scope))
+        .catch((err) => console.error("[SW] 등록 실패:", err));
+    }
+  }, []);
+
+  return null;
+}
